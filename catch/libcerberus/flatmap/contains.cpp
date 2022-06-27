99b2343d22f79f80cf3a607a5f5309a381bb2682
vaskozlov <vaskozlov3940@gmail.com>
2022-06-27 13:45:56 +0500

    Useless pair has been deleted constant size flatmap

---
+++ catch/libcerberus/flatmap/contains.cpp
@@ -0,0 +1,21 @@
+#include <cerberus/debug/debug.hpp>
+#include <cerberus/flatmap.hpp>
+
+// NOLINTBEGIN
+
+using Flatmap = cerb::Flatmap<int, int, 10>;
+template class cerb::Flatmap<int, int, 10>;
+
+STATIC_VARIABLE Flatmap flatmap{ { 10, 20 }, { 20, 30 }, { 30, 40 } };
+
+static_assert(flatmap.size() == 3);
+static_assert(flatmap.capacity() == 10);
+
+static_assert(flatmap.contains(10));
+static_assert(flatmap.contains(20));
+static_assert(flatmap.contains(30));
+
+static_assert(!flatmap.contains(0));
+static_assert(!flatmap.contains(40));
+
+// NOLINTEND
---
+++ catch/libcerberus/flatmap/indexing.cpp
@@ -0,0 +1,24 @@
+#include <cerberus/debug/debug.hpp>
+#include <cerberus/flatmap.hpp>
+
+// NOLINTBEGIN
+
+using Flatmap = cerb::Flatmap<int, int, 10>;
+
+DEBUG_SCOPE
+{
+    STATIC_VARIABLE Flatmap flatmap{ { 10, 20 }, { 20, 30 }, { 30, 40 } };
+
+    static_assert(flatmap.size() == 3);
+    static_assert(flatmap.capacity() == 10);
+
+    static_assert(flatmap[10] == 20);
+    static_assert(flatmap[20] == 30);
+    static_assert(flatmap[30] == 40);
+
+    ERROR_EXPECTED(UNUSED_DECL flatmap.at(40), std::out_of_range, "key not found");
+
+    return {};
+};
+
+// NOLINTEND
