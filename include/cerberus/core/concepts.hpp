170ea66e75e87c9c5b4b8498f75b3bf7abc0b8c0
vaskzolov <vaskozlov3940@gmail.com>
2022-07-03 17:03:29 +0500

    Fix in concepts

--- include/cerberus/core/concepts.hpp
+++ include/cerberus/core/concepts.hpp
@@ -16,11 +16,15 @@ namespace cerb
     concept Exception = std::is_base_of_v<std::exception, T>;
 
     template<typename T>
-    concept Pairable = requires(T pair)
-    {
-        pair.first;
-        pair.second;
-    };
+    concept CharacterLiteral =
+        std::is_same_v<T, char> || std::is_same_v<T, unsigned char> || std::is_same_v<T, char8_t> ||
+        std::is_same_v<T, char16_t> || std::is_same_v<T, char32_t> || std::is_same_v<T, wchar_t>;
+
+    template<typename T>
+    concept Pairable = requires(T pair) {
+                           pair.first;
+                           pair.second;
+                       };
 }// namespace cerb
 
 #endif /* CERBERUS_PROJECT_CONCEPTS_HPP */
