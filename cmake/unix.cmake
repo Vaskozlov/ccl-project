12d8aa549d112889a22b85ac3ce846ff6f276990
vaskozlov <vaskozlov3940@gmail.com>
2022-07-16 08:55:29 +0500

    New sanitizer options

--- cmake/unix.cmake
+++ cmake/unix.cmake
@@ -8,7 +8,12 @@ if (CERBERUS_STRICT_COMPILATION)
     add_compile_options(-Werror)
 endif ()
 
-if (USE_SANITIZERS)
+if (USE_ADDRESS_SANITIZER)
     add_compile_options(-fsanitize=address,undefined,leak)
     add_link_options(-fsanitize=address,undefined,leak)
+endif ()
+
+if (USE_THREAD_SANITIZER)
+    add_compile_options(-fsanitize=thread)
+    add_link_options(-fsanitize=thread)
 endif ()
