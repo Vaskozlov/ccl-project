6c38cf114ea8138fbc7459f809b327b377b220b6
vaskozlov <vaskozlov3940@gmail.com>
2022-07-28 09:07:11 +0500

    Test for terminal fix

--- catch/libcerberus/lex/dot_item/dot_item/terminal.cpp
+++ catch/libcerberus/lex/dot_item/dot_item/terminal.cpp
@@ -9,10 +9,28 @@ using namespace cerb::lex::dot_item;
 RUNTIME_TEST
 {
     auto shared = AnalysisShared<char>{};
-    auto dot_item = DotItem<char>(TextIterator<char>{ "[]'+'" }, 0, shared);
+    auto dot_item = DotItem<char>(TextIterator<char>{ "'+'" }, 0, shared);
 
     assertEqual(shared.terminals.size(), 1_ZU);
     assertEqual(shared.terminals.matches("+").success, true);
+
+    return {};
+}
+();
+
+
+// NOLINTNEXTLINE
+RUNTIME_TEST
+{
+    auto shared = AnalysisShared<char>{};
+
+    ERROR_EXPECTED(DotItem<char>(TextIterator<char>{ "[]'+'" }, 0, shared);
+                   , TextIteratorException<char>,
+                   "Error occurred at: , line: 1, column: 3. Error message: dot item with terminal "
+                   "must be empty\n"
+                   "[]'+'\n"
+                   "  ^\n"
+                   "Suggestion: delete other items");
 
     return {};
 }
