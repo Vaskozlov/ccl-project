37d09f7198a08297920362c470568e891269586c
vaskozlov <vaskozlov3940@gmail.com>
2022-08-21 12:46:29 +0500

    More usage of PCH. Clang compilation with libcpp has been added to GitHub actions

---
+++ pch.hpp
@@ -0,0 +1,32 @@
+#ifndef CERBERUS_PROJECT_PCH_HPP
+#define CERBERUS_PROJECT_PCH_HPP
+
+#include <array>
+#include <bitset>
+#include <cinttypes>
+#include <cmath>
+#include <deque>
+#include <initializer_list>
+#include <map>
+#include <optional>
+#include <set>
+#include <string>
+#include <string_view>
+#include <variant>
+#include <vector>
+
+#include <algorithm>
+#include <concepts>
+#include <cstddef>
+#include <exception>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <version>
+
+#include <fmt/format.h>
+
+#include <boost/container/small_vector.hpp>
+#include <boost/test/unit_test.hpp>
+
+#endif /* CERBERUS_PROJECT_PCH_HPP */
