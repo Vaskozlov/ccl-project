0653828ae3bc21c44191809516b05726a7544441
vaskozlov <vaskozlov3940@gmail.com>
2022-11-12 17:28:22 +0500

    as conversion operator

---
+++ catch/ccl/core/as.cpp
@@ -0,0 +1,75 @@
+#include "ccl/core/as.hpp"
+#include <boost/test/unit_test_suite.hpp>
+#include <ccl/core/as.hpp>
+#include <ccl/debug/debug_file.hpp>
+#include <type_traits>
+
+using namespace ccl;
+
+// NOLINTBEGIN
+
+struct A
+{
+    A() = default;
+
+    virtual ~A() = default;
+
+    virtual auto getId() const -> int
+    {
+        return 10;
+    }
+};
+
+struct B : A
+{
+    auto getId() const -> int override
+    {
+        return 20;
+    }
+};
+
+BOOST_AUTO_TEST_SUITE(AsConvertion)
+
+BOOST_AUTO_TEST_CASE(TestAsOnInts)
+{
+    static_assert(std::is_same_v<i64, decltype(as<i64>(10))>);
+    BOOST_ASSERT(as<i64>(10) == 10);
+}
+
+BOOST_AUTO_TEST_CASE(TestAsOnFloats)
+{
+    static_assert(std::is_same_v<f32, decltype(as<f32>(10))>);
+    BOOST_ASSERT(as<f32>(10) == 10.0F);
+}
+
+BOOST_AUTO_TEST_CASE(TestAsOnPointer)
+{
+    auto b = B{};
+
+    auto *a = as<A *>(&b);
+    static_assert(std::is_same_v<decltype(a), A *>);
+
+    auto *b_from_a = as<B *>(a);
+    static_assert(std::is_same_v<decltype(b_from_a), B *>);
+
+    BOOST_ASSERT(a->getId() == 20);
+    BOOST_ASSERT(b_from_a->getId() == 20);
+}
+
+BOOST_AUTO_TEST_CASE(TestAsOnReferences)
+{
+    auto b = B{};
+
+    auto &a = as<A &>(b);
+    static_assert(std::is_same_v<decltype(a), A &>);
+
+    auto &b_from_a = as<B &>(a);
+    static_assert(std::is_same_v<decltype(b_from_a), B &>);
+
+    BOOST_ASSERT(a.getId() == 20);
+    BOOST_ASSERT(b_from_a.getId() == 20);
+}
+
+// NOLINTEND
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ include/ccl/core/as.hpp
@@ -0,0 +1,46 @@
+#ifndef CCL_PROJECT_AS_HPP
+#define CCL_PROJECT_AS_HPP
+
+#include <ccl/core/defines.hpp>
+#include <concepts>
+#include <type_traits>
+#include <utility>
+
+namespace ccl
+{
+    template<typename T, typename U>
+    CCL_DECL auto as(U &&value) -> T
+    {
+        return static_cast<T>(std::forward<U>(value));
+    }
+
+    template<typename T, typename U>
+    CCL_DECL auto as(U &value) -> T
+        requires(std::is_reference_v<T> && std::derived_from<U, std::remove_reference_t<T>>)
+    {
+        return static_cast<T>(value);
+    }
+
+    template<typename T, typename U>
+    CCL_DECL auto as(U &value) -> T
+        requires(std::is_reference_v<T> && !std::derived_from<U, std::remove_reference_t<T>>)
+    {
+        return dynamic_cast<T>(value);
+    }
+
+    template<typename T, typename U>
+    CCL_DECL auto as(U *value) -> T
+        requires(std::is_pointer_v<T> && std::derived_from<U, std::remove_pointer_t<T>>)
+    {
+        return static_cast<T>(value);
+    }
+
+    template<typename T, typename U>
+    CCL_DECL auto as(U *value) -> T
+        requires(std::is_pointer_v<T> && !std::derived_from<U, std::remove_pointer_t<T>>)
+    {
+        return dynamic_cast<T>(value);
+    }
+}// namespace ccl
+
+#endif /* CCL_PROJECT_AS_HPP */
