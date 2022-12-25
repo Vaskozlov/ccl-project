479d2d8054bbf8abd3c963e37edb649a8c43a12e
vaskozlov <vaskozlov3940@gmail.com>
2022-12-25 10:34:07 +0500

    Raii should be constexpr

--- include/ccl/core/raii.hpp
+++ include/ccl/core/raii.hpp
@@ -15,7 +15,7 @@ namespace ccl
         Deleter deleter;
 
     public:
-        Raii(Constructor &&t_constructor, Deleter &&t_deleter)
+        constexpr Raii(Constructor &&t_constructor, Deleter &&t_deleter)
           : constructor{std::forward<Constructor>(t_constructor)}
           , deleter{std::forward<Deleter>(t_deleter)}
         {
@@ -25,7 +25,7 @@ namespace ccl
         Raii(const Raii &) = delete;
         Raii(Raii &&) noexcept = default;
 
-        ~Raii()
+        constexpr ~Raii()
         {
             deleter();
         }
