f1da6bf3d726fb5b28c4f557dade6669dc99958f
vaskozlov <vaskozlov3940@gmail.com>
2022-12-25 10:31:05 +0500

    float equal fix

--- catch/ccl/core/as.cpp
+++ catch/ccl/core/as.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test_suite.hpp>
 #include <ccl/core/as.hpp>
 #include <ccl/debug/debug_file.hpp>
+#include <limits>
 #include <type_traits>
 
 using namespace ccl;
@@ -38,7 +39,7 @@ BOOST_AUTO_TEST_CASE(TestAsOnInts)
 BOOST_AUTO_TEST_CASE(TestAsOnFloats)
 {
     static_assert(std::is_same_v<f32, decltype(as<f32>(10))>);
-    BOOST_ASSERT(as<f32>(10) == 10.0F);
+    BOOST_ASSERT((as<f32>(10) - 10.0F) <= std::numeric_limits<f32>::epsilon());
 }
 
 BOOST_AUTO_TEST_CASE(TestAsOnPointer)
