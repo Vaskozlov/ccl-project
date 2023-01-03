d01378019b0880cd7ffb4d6ed285cbe14f346dfd
vaskozlov <vaskozlov3940@gmail.com>
2023-01-03 15:22:43 +0500

    Basic code generator

---
+++ include/ccl/codegen/basic_codegen.hpp
@@ -0,0 +1,86 @@
+#ifndef CCL_PROJECT_BASIC_CODEGEN_HPP
+#define CCL_PROJECT_BASIC_CODEGEN_HPP
+
+#include <ccl/const_string.hpp>
+#include <ccl/string_view.hpp>
+
+namespace ccl::codegen
+{
+    class BasicCodeGenerator
+    {
+    public:
+        struct CCL_TRIVIAL_ABI PushScope : std::true_type
+        {
+        };
+
+        struct CCL_TRIVIAL_ABI PopScope : std::false_type
+        {
+        };
+
+        struct CCL_TRIVIAL_ABI Endl : std::true_type
+        {
+        };
+
+        struct CCL_TRIVIAL_ABI ScopeSize
+        {
+            size_t size = 4;
+        };
+
+    private:
+        std::string generatedCode{};
+        size_t scopesCounter = 0;
+        size_t scopeSize = 4;
+
+    public:
+        BasicCodeGenerator() = default;
+
+        auto getCode() const noexcept -> const std::string &
+        {
+            return generatedCode;
+        }
+
+        auto operator<<(ScopeSize scope_size) -> BasicCodeGenerator &;
+        auto operator<<(PushScope /* unused */) -> BasicCodeGenerator &;
+        auto operator<<(PopScope /* unused */) -> BasicCodeGenerator &;
+
+        auto operator<<(char character) -> BasicCodeGenerator &;
+        auto operator<<(string_view string) -> BasicCodeGenerator &;
+        auto operator<<(std::string_view string) -> BasicCodeGenerator &;
+        auto operator<<(const std::string &string) -> BasicCodeGenerator &;
+
+        auto operator<<(Endl /* unused */) -> BasicCodeGenerator &
+        {
+            newLine();
+            return *this;
+        }
+
+        template<size_t Size>
+        auto operator<<(const ConstString<Size> &string) -> BasicCodeGenerator &
+        {
+            return *this << as<string_view>(string);
+        }
+
+        template<size_t N>
+        auto operator<<(const char (&character_array)[N]) -> BasicCodeGenerator &// NOLINT
+        {
+            return *this << std::string_view{
+                       std::forward<const char(&)[N]>(character_array)};// NOLINT
+        }
+
+    private:
+        auto newLine() -> void;
+        auto addScope(size_t scopes_count) -> void;
+    };
+
+    inline auto setScopeSize(size_t size) noexcept -> BasicCodeGenerator::ScopeSize
+    {
+        return {.size = size};
+    }
+
+    constexpr inline auto endl = '\n';
+    constexpr inline auto push_scope = BasicCodeGenerator::PushScope{};
+    constexpr inline auto pop_scope = BasicCodeGenerator::PopScope{};
+
+}// namespace ccl::codegen
+
+#endif /* CCL_PROJECT_BASIC_CODEGEN_HPP */
---
+++ src/ccl/codegen/basic_codegen.cpp
@@ -0,0 +1,97 @@
+#include "ccl/codegen/basic_codegen.hpp"
+#include "ccl/flatmap.hpp"
+#include <stdexcept>
+
+namespace ccl::codegen
+{
+    constexpr bool HasStdSplit =
+        requires(std::string_view string) { std::views::split(string, '\n'); };
+
+    auto BasicCodeGenerator::newLine() -> void
+    {
+        generatedCode.push_back('\n');
+
+        addScope(scopesCounter);
+    }
+
+    auto BasicCodeGenerator::addScope(size_t scopes_count) -> void
+    {
+        for (size_t i = 0; i < scopes_count * scopeSize; i += 4) {
+            generatedCode.append("    ");
+        }
+
+        for (size_t i = 0; i != scopes_count * scopeSize % 4; ++i) {
+            generatedCode.push_back(' ');
+        }
+    }
+
+    auto BasicCodeGenerator::operator<<(ScopeSize scope_size) -> BasicCodeGenerator &
+    {
+        if (0 == scope_size.size) {
+            throw std::logic_error("Scope size can not be equal to zero.");
+        }
+
+        scopeSize = scope_size.size;
+        return *this;
+    }
+
+    auto BasicCodeGenerator::operator<<(PushScope /* unused */) -> BasicCodeGenerator &
+    {
+        ++scopesCounter;
+        addScope(1);
+        return *this;
+    }
+
+    auto BasicCodeGenerator::operator<<(PopScope /* unused */) -> BasicCodeGenerator &
+    {
+        if (scopesCounter == 0) {
+            throw std::runtime_error("Cannot pop scope, because there are not any of them");
+        }
+
+        --scopesCounter;
+        return *this;
+    }
+
+    auto BasicCodeGenerator::operator<<(char character) -> BasicCodeGenerator &
+    {
+        if (character == '\n') {
+            newLine();
+        } else {
+            generatedCode.push_back(character);
+        }
+
+        return *this;
+    }
+
+    auto BasicCodeGenerator::operator<<(string_view string) -> BasicCodeGenerator &
+    {
+        return *this << as<std::string_view>(string);
+    }
+
+    auto BasicCodeGenerator::operator<<(std::string_view string) -> BasicCodeGenerator &
+    {
+        if constexpr (HasStdSplit) {
+            auto need_to_add_new_line = false;
+
+            for (auto &&word : std::views::split(string, '\n')) {
+                if (need_to_add_new_line) {
+                    newLine();
+                }
+
+                need_to_add_new_line = true;
+                generatedCode.append(std::string_view{word});
+            }
+        } else {
+            for (auto character : string) {
+                *this << character;
+            }
+        }
+
+        return *this;
+    }
+
+    auto BasicCodeGenerator::operator<<(const std::string &string) -> BasicCodeGenerator &
+    {
+        return *this << as<std::string_view>(string);
+    }
+}// namespace ccl::codegen
