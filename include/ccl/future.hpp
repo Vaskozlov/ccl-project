cb2943acf4c93a8f8d34adb69efddd80b8b5103d
vaskozlov <vaskozlov3940@gmail.com>
2023-03-05 19:44:43 +0500

    async should be inline

--- include/ccl/future.hpp
+++ include/ccl/future.hpp
@@ -1,12 +1,13 @@
 #ifndef CCL_PROJECT_FUTURE_HPP
 #define CCL_PROJECT_FUTURE_HPP
 
+#include <ccl/ccl.hpp>
 #include <future>
 
 namespace ccl
 {
     template<typename Function, typename... Ts>
-    [[nodiscard]] auto async(Function &&function, Ts &&...args) -> decltype(auto)
+    [[nodiscard]] CCL_INLINE auto async(Function &&function, Ts &&...args) -> decltype(auto)
     {
         return std::async(
             std::launch::async, std::forward<Function>(function), std::forward<Ts>(args)...);
