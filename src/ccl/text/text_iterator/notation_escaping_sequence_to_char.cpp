2369f8b538c7e35c9ec6ab62c742ac68993af306
vaskozlov <vaskozlov3940@gmail.com>
2023-05-09 09:15:52 +0500

    Some renaming

--- src/ccl/text/text_iterator/notation_escaping_sequence_to_char.cpp
+++ src/ccl/text/text_iterator/notation_escaping_sequence_to_char.cpp
@@ -2,9 +2,9 @@
 
 namespace ccl::text
 {
-    using NotationEscapingSymbolizer = TextIterator::NotationEscapingSequenceToChar;
+    using NotationEscapingSequenceToChar = TextIterator::NotationEscapingSequenceToChar;
 
-    NotationEscapingSymbolizer::NotationEscapingSequenceToChar(
+    NotationEscapingSequenceToChar::NotationEscapingSequenceToChar(
         TextIterator &text_iterator,
         u16 maximum_symbols,
         u16 notation_power,
@@ -18,7 +18,7 @@ namespace ccl::text
         calculateResult();
     }
 
-    auto NotationEscapingSymbolizer::calculateResult() -> void
+    auto NotationEscapingSequenceToChar::calculateResult() -> void
     {
         auto chars_count = as<size_t>(0);
 
@@ -38,7 +38,7 @@ namespace ccl::text
         checkAllCharsUsage(chars_count);
     }
 
-    auto NotationEscapingSymbolizer::createSuggestionNotEnoughChars(size_t chars_count) const
+    auto NotationEscapingSequenceToChar::createSuggestionNotEnoughChars(size_t chars_count) const
         -> std::string
     {
         auto suggestion_message = as<std::string>(textIterator.getWorkingLine());
@@ -46,7 +46,7 @@ namespace ccl::text
         return suggestion_message;
     }
 
-    auto NotationEscapingSymbolizer::insertExtraZerosToNotEnoughMessage(
+    auto NotationEscapingSequenceToChar::insertExtraZerosToNotEnoughMessage(
         size_t chars_count,
         std::string &message) const -> void
     {
@@ -57,25 +57,25 @@ namespace ccl::text
         message.insert(insertion_position, insertion_size, '0');
     }
 
-    auto NotationEscapingSymbolizer::checkNotation() const -> void
+    auto NotationEscapingSequenceToChar::checkNotation() const -> void
     {
-        CCL_ASSERT(land(notationPower > 0, notationPower <= 4));
+        CCL_ASSERT(notationPower > 0 && notationPower <= 4);
     }
 
-    auto NotationEscapingSymbolizer::isOutOfNotation(char32_t chr) const -> bool
+    auto NotationEscapingSequenceToChar::isOutOfNotation(char32_t chr) const -> bool
     {
         return !HexadecimalCharsToInt.contains(chr) ||
                HexadecimalCharsToInt.at(chr) >= pow2(notationPower);
     }
 
-    auto NotationEscapingSymbolizer::checkAllCharsUsage(size_t chars_count) const -> void
+    auto NotationEscapingSequenceToChar::checkAllCharsUsage(size_t chars_count) const -> void
     {
-        if (land(areAllCharsRequired, chars_count != maximumSymbols)) {
+        if (areAllCharsRequired && chars_count != maximumSymbols) {
             throwNotEnoughCharsException(chars_count);
         }
     }
 
-    auto NotationEscapingSymbolizer::throwNotEnoughCharsException(size_t chars_count) const -> void
+    auto NotationEscapingSequenceToChar::throwNotEnoughCharsException(size_t chars_count) const -> void
     {
         auto exception_message = fmt::format(
             "expected {} characters, but only {} of them were provided", maximumSymbols,
