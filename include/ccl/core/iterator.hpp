b8d29b80344951edc6071e048d8c395531b6953c
vaskozlov <vaskozlov3940@gmail.com>
2023-10-07 21:34:58 +0300

    reverting changes in iterator.hpp

--- include/ccl/core/iterator.hpp
+++ include/ccl/core/iterator.hpp
@@ -14,55 +14,55 @@ namespace ccl
             return static_cast<const T &>(*this);
         }
 
-        CCL_DECL auto begin() const
-            CCL_NOEXCEPT_IF(std::declval<const T>().begin()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto begin() const noexcept(noexcept(std::declval<const T>().begin()))
+            -> decltype(auto)
         {
             return std::begin(toParent());
         }
 
-        CCL_DECL auto end() const
-            CCL_NOEXCEPT_IF(std::declval<const T>().end()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto end() const noexcept(noexcept(std::declval<const T>().end()))
+            -> decltype(auto)
         {
             return std::end(toParent());
         }
 
     public:
-        CCL_DECL auto cbegin() const CCL_NOEXCEPT_IF(begin()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto cbegin() const noexcept(noexcept(begin())) -> decltype(auto)
         {
             return begin();
         }
 
-        CCL_DECL auto cend() const CCL_NOEXCEPT_IF(end()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto cend() const noexcept(noexcept(end())) -> decltype(auto)
         {
             return end();
         }
 
-        CCL_DECL auto rbegin() CCL_NOEXCEPT_IF(end()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto rbegin() noexcept(noexcept(end())) -> decltype(auto)
         {
             return std::reverse_iterator{end()};
         }
 
-        CCL_DECL auto rend() CCL_NOEXCEPT_IF(begin()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto rend() noexcept(noexcept(begin())) -> decltype(auto)
         {
             return std::reverse_iterator{begin()};
         }
 
-        CCL_DECL auto rbegin() const CCL_NOEXCEPT_IF(end()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto rbegin() const noexcept(noexcept(end())) -> decltype(auto)
         {
             return std::reverse_iterator{end()};
         }
 
-        CCL_DECL auto rend() const CCL_NOEXCEPT_IF(begin()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto rend() const noexcept(noexcept(begin())) -> decltype(auto)
         {
             return std::reverse_iterator{begin()};
         }
 
-        CCL_DECL auto crbegin() const CCL_NOEXCEPT_IF(rbegin()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto crbegin() const noexcept(noexcept(rbegin())) -> decltype(auto)
         {
             return rbegin();
         }
 
-        CCL_DECL auto crend() const CCL_NOEXCEPT_IF(rend()) CCL_LIFETIMEBOUND->decltype(auto)
+        CCL_DECL auto crend() const noexcept(noexcept(rend())) -> decltype(auto)
         {
             return rend();
         }
