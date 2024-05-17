c98edabe343f3557216f24f4df01a37fa60fbf5b
vaskozlov <vaskozlov3940@gmail.com>
2024-05-17 15:06:09 +0300

    feat: fetch content instead of git module

---
+++ cmake/ImportISL.cmake
@@ -0,0 +1,16 @@
+include(FetchContent)
+
+FetchContent_Declare(
+        isl
+        GIT_REPOSITORY https://github.com/Vaskozlov/ISL.git
+        GIT_TAG origin/main
+)
+
+FetchContent_MakeAvailable(isl)
+
+include(${isl_SOURCE_DIR}/cmake/CompilerOptions.cmake)
+include(${isl_SOURCE_DIR}/cmake/InterproceduralOptimization.cmake)
+include(${isl_SOURCE_DIR}/cmake/Libraries.cmake)
+include(${isl_SOURCE_DIR}/cmake/ProjectOptions.cmake)
+
+include_directories(${isl_SOURCE_DIR}/include)
--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -3,5 +3,3 @@ find_package(Catch2 CONFIG QUIET)
 find_package(Threads REQUIRED)
 find_package(fmt CONFIG REQUIRED)
 find_package(cxxopts CONFIG REQUIRED)
-
-include_directories(ISL/include)
