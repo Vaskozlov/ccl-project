ded32716fbb899c72c55be3604a5bdb4ff77cc40
vaskozlov <vaskozlov3940@gmail.com>
2024-07-14 15:41:54 +0500

    feat: rely on isl catch2 instead of having own version

--- cmake/ImportCatch2.cmake
+++
@@ -1,7 +0,0 @@
-FetchContent_Declare(
-        Catch2
-        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-        GIT_TAG devel
-)
-
-FetchContent_MakeAvailable(Catch2)
--- cmake/ImportISL.cmake
+++ cmake/ImportISL.cmake
@@ -1,5 +1,3 @@
-include(FetchContent)
-
 FetchContent_Declare(
         isl
         GIT_REPOSITORY https://github.com/Vaskozlov/ISL.git
@@ -10,7 +8,6 @@ FetchContent_MakeAvailable(isl)
 
 include(${isl_SOURCE_DIR}/cmake/CompilerOptions.cmake)
 include(${isl_SOURCE_DIR}/cmake/InterproceduralOptimization.cmake)
-include(${isl_SOURCE_DIR}/cmake/Libraries.cmake)
 include(${isl_SOURCE_DIR}/cmake/ProjectOptions.cmake)
 
 include_directories(${isl_SOURCE_DIR}/include)
