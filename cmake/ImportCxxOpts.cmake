e21b754d27494f829d214c9b4edb5b58f56a08b6
vaskozlov <vaskozlov3940@gmail.com>
2024-07-16 15:10:16 +0500

    fix: excluding dependencies

--- cmake/ImportCxxOpts.cmake
+++ cmake/ImportCxxOpts.cmake
@@ -2,6 +2,7 @@ FetchContent_Declare(
         cxxopts
         GIT_REPOSITORY https://github.com/jarro2783/cxxopts.git
         GIT_TAG master
+        EXCLUDE_FROM_ALL
 )
 
 FetchContent_MakeAvailable(cxxopts)
--- cmake/ImportFMT.cmake
+++ cmake/ImportFMT.cmake
@@ -2,6 +2,7 @@ FetchContent_Declare(
         fmt
         GIT_REPOSITORY https://github.com/fmtlib/fmt.git
         GIT_TAG master
+        EXCLUDE_FROM_ALL
 )
 
 FetchContent_MakeAvailable(fmt)
