e4d3b6e3c319a1d6db6cd9c2701b68b1e0ba0c0b
vaskozlov <vaskozlov3940@gmail.com>
2024-11-18 20:49:49 +0300

    fix: template parameter

--- src/ast-lang-2/interpreter/builtin/zero_argument_functions.cpp
+++ src/ast-lang-2/interpreter/builtin/zero_argument_functions.cpp
@@ -4,7 +4,6 @@
 
 namespace astlang2::interpreter
 {
-    template<>
     static auto createZeroArgumentFunction(
         function::FunctionsHolder &functions_holder, std::string function_name,
         ts::Type *return_type, std::invocable auto &&function) -> void
@@ -20,6 +19,68 @@ namespace astlang2::interpreter
             });
 
         functions_holder.addFunction(std::move(identification), std::move(builtin_function));
+    }
+
+    static auto constructVec2(double x, double y, const Interpreter &interpreter)
+    {
+        auto vec2 = Instance{
+            {
+                "x",
+                Value{
+                    .object = std::make_shared<double>(x),
+                    .type = interpreter.getDouble(),
+                    .valueType = ValueType::L_VALUE,
+                },
+            },
+            {
+                "y",
+                Value{
+                    .object = std::make_shared<double>(y),
+                    .type = interpreter.getDouble(),
+                    .valueType = ValueType::L_VALUE,
+                },
+            },
+        };
+
+        return Value{
+            .object = std::make_shared<Instance>(std::move(vec2)),
+            .type = interpreter.getVec2(),
+        };
+    }
+
+    static auto constructVec3(double x, double y, double z, const Interpreter &interpreter)
+    {
+        auto vec3 = Instance{
+            {
+                "x",
+                Value{
+                    .object = std::make_shared<double>(x),
+                    .type = interpreter.getDouble(),
+                    .valueType = ValueType::L_VALUE,
+                },
+            },
+            {
+                "y",
+                Value{
+                    .object = std::make_shared<double>(y),
+                    .type = interpreter.getDouble(),
+                    .valueType = ValueType::L_VALUE,
+                },
+            },
+            {
+                "z",
+                Value{
+                    .object = std::make_shared<double>(z),
+                    .type = interpreter.getDouble(),
+                    .valueType = ValueType::L_VALUE,
+                },
+            },
+        };
+
+        return Value{
+            .object = std::make_shared<Instance>(std::move(vec3)),
+            .type = interpreter.getVec3(),
+        };
     }
 
     auto defaultInitializeFunctionsHolderWithZeroArgumentFunctions(
@@ -41,89 +102,61 @@ namespace astlang2::interpreter
 
         functions_holder.addFunction(
             function::FunctionIdentification{
+                .name = "vec2", .arguments = {}, .returnType = type_system.getVec2()},
+            std::make_shared<function::BuiltinFunction>(
+                isl::SmallVector<std::string, 2>{}, [](const Interpreter &interpreter) {
+                    return constructVec2(0.0, 0.0, interpreter);
+                }));
+
+        functions_holder.addFunction(
+            function::FunctionIdentification{
                 .name = "vec2",
-                .arguments = {type_system.getDouble(), type_system.getDouble()},
+                .arguments =
+                    {
+                        type_system.getDouble(),
+                        type_system.getDouble(),
+                    },
                 .returnType = type_system.getVec2()},
             std::make_shared<function::BuiltinFunction>(
-                isl::SmallVector<std::string, 2>{"x", "y"},
-                [](Interpreter &interpreter) {
-                    auto x_ptr = interpreter.read("x");
-                    auto y_ptr = interpreter.read("y");
-                    auto x_value = *static_cast<double *>(x_ptr.object.get());
-                    auto y_value = *static_cast<double *>(y_ptr.object.get());
+                isl::SmallVector<std::string, 2>{"x", "y"}, [](Interpreter &interpreter) {
+                    const auto x_ptr = interpreter.read("x");
+                    const auto y_ptr = interpreter.read("y");
 
-                    auto vec2 = Instance{
-                        {
-                            "x",
-                            Value{
-                                .object = std::make_shared<double>(x_value),
-                                .type = interpreter.getDouble(),
-                                .valueType = ValueType::L_VALUE,
-                            },
-                        },
-                        {
-                            "y",
-                            Value{
-                                .object = std::make_shared<double>(y_value),
-                                .type = interpreter.getDouble(),
-                                .valueType = ValueType::L_VALUE,
-                            },
-                        },
-                    };
+                    const auto x_value = *static_cast<double *>(x_ptr.object.get());
+                    const auto y_value = *static_cast<double *>(y_ptr.object.get());
 
-                    return Value{
-                        .object = std::make_shared<Instance>(std::move(vec2)),
-                        .type = interpreter.getVec2(),
-                    };
+                    return constructVec2(x_value, y_value, interpreter);
+                }));
+
+        functions_holder.addFunction(
+            function::FunctionIdentification{
+                .name = "vec3", .arguments = {}, .returnType = type_system.getVec3()},
+            std::make_shared<function::BuiltinFunction>(
+                isl::SmallVector<std::string, 2>{}, [](const Interpreter &interpreter) {
+                    return constructVec3(0.0, 0.0, 0.0, interpreter);
                 }));
 
         functions_holder.addFunction(
             function::FunctionIdentification{
                 .name = "vec3",
                 .arguments =
-                    {type_system.getDouble(), type_system.getDouble(), type_system.getDouble()},
+                    {
+                        type_system.getDouble(),
+                        type_system.getDouble(),
+                        type_system.getDouble(),
+                    },
                 .returnType = type_system.getVec3()},
             std::make_shared<function::BuiltinFunction>(
-                isl::SmallVector<std::string, 2>{"x", "y", "z"},
-                [](Interpreter &interpreter) {
-                    auto x_ptr = interpreter.read("x");
-                    auto y_ptr = interpreter.read("y");
-                    auto z_ptr = interpreter.read("z");
-                    auto x_value = *static_cast<double *>(x_ptr.object.get());
-                    auto y_value = *static_cast<double *>(y_ptr.object.get());
-                    auto z_value = *static_cast<double *>(z_ptr.object.get());
+                isl::SmallVector<std::string, 2>{"x", "y", "z"}, [](Interpreter &interpreter) {
+                    const auto x_ptr = interpreter.read("x");
+                    const auto y_ptr = interpreter.read("y");
+                    const auto z_ptr = interpreter.read("z");
 
-                    auto vec3 = Instance{
-                        {
-                            "x",
-                            Value{
-                                .object = std::make_shared<double>(x_value),
-                                .type = interpreter.getDouble(),
-                                .valueType = ValueType::L_VALUE,
-                            },
-                        },
-                        {
-                            "y",
-                            Value{
-                                .object = std::make_shared<double>(y_value),
-                                .type = interpreter.getDouble(),
-                                .valueType = ValueType::L_VALUE,
-                            },
-                        },
-                        {
-                            "z",
-                            Value{
-                                .object = std::make_shared<double>(z_value),
-                                .type = interpreter.getDouble(),
-                                .valueType = ValueType::L_VALUE,
-                            },
-                        },
-                    };
+                    const auto x_value = *static_cast<double *>(x_ptr.object.get());
+                    const auto y_value = *static_cast<double *>(y_ptr.object.get());
+                    const auto z_value = *static_cast<double *>(z_ptr.object.get());
 
-                    return Value{
-                        .object = std::make_shared<Instance>(std::move(vec3)),
-                        .type = interpreter.getVec3(),
-                    };
+                    return constructVec3(x_value, y_value, z_value, interpreter);
                 }));
     }
 }// namespace astlang2::interpreter
