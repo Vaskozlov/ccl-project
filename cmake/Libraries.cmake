1174cb56910eb2bfc2f728127724539026bc5634
vaskozlov <vaskozlov3940@gmail.com>
2025-03-01 12:51:11 +0300

    feat: specific versions of dependencies

--- cmake/Libraries.cmake
+++ cmake/Libraries.cmake
@@ -4,26 +4,26 @@ find_package(Threads REQUIRED)
 
 CPMFindPackage(
         NAME fmt
-        GIT_TAG master
         GITHUB_REPOSITORY fmtlib/fmt
+        GIT_TAG 577fd3be883accf8629423ed77fcca8a24bccee2
 )
 
 CPMFindPackage(
         NAME Catch2
         GITHUB_REPOSITORY catchorg/Catch2
-        GIT_TAG devel
+        GIT_TAG 914aeecfe23b1e16af6ea675a4fb5dbd5a5b8d0a
 )
 
-CPMFindPackage(
+CPMAddPackage(
         NAME unordered_dense
         GITHUB_REPOSITORY martinus/unordered_dense
-        GIT_TAG main
+        GIT_TAG 73f3cbb237e84d483afafc743f1f14ec53e12314
 )
 
 CPMFindPackage(
         NAME cxxopts
         GITHUB_REPOSITORY jarro2783/cxxopts
-        GIT_TAG master
+        GIT_TAG 573ec27d7224a8a0866c1889737d355b8e4155d7
         OPTIONS
         "CXXOPTS_BUILD_EXAMPLES Off"
         "CXXOPTS_BUILD_TESTS Off"
@@ -32,14 +32,14 @@ CPMFindPackage(
 CPMAddPackage(
         NAME isl
         GITHUB_REPOSITORY Vaskozlov/ISL
-        GIT_TAG main
+        GIT_TAG fe4600553b320d0937bf08abc47bf159f65e109d
 )
 
 if (CCL_INCLUDE_BENCHMARK)
     CPMAddPackage(
             NAME benchmark
             GITHUB_REPOSITORY google/benchmark
-            GIT_TAG main
+            GIT_TAG ff5c94d860dcffd4f4159edf79d805bcfaca3cb8
             OPTIONS
             "BENCHMARK_ENABLE_TESTING OFF"
             "BENCHMARK_USE_LIBCXX ${CCL_LIBCPP}"
